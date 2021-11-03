929de7b182e6394655161cbacde42bad199ea842
Chris A <chrisa@tutamail.com>
2021-11-03 10:51:29 +0100

    css and background color

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import logo from './PIVX-Logo.png';
+
 import './App.css';
 import { Content } from './components/Content'
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="PIVX logo" padding="10"/>
       <Content />
     </div>
   );
